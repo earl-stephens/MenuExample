e194d29876ad2510a816fa85009e1ae7ccd18f6a
earl-stephens <stephensearl@hotmail.com>
2021-12-24 22:01:01 -0700

    Complete second coding challenge

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -14,5 +14,19 @@ public class Menu {
 			System.out.printf("%d. %s\n", (i + 1), menuOptions[i]);
 		}
 	};
+	
+	String get() {
+		String optionsText = "Options: \n";
+		for(int j = 0; j < menuOptions.length; j++) {
+			/* First attempt
+			String number = String.valueOf(j + 1);
+			optionsText += (number + ". " + menuOptions[j] + "\n");
+			*/
+			
+			//use of String.format
+			optionsText += String.format("%d. %s\n", (j + 1), menuOptions[j]);
+		}
+		return optionsText;
+	};
 
 }
